04ecc9eae86bba1fea4c638f4925595a03cbdfe5
shadrick <barna@mikandi.com>
2017-11-17 15:27:35 +0200

    Revert "Increased overall timeout at tester"
    This reverts commit 609f52eccd9ba47d5a478ca6a55353b2131fbd40.

--- src/tenta-dns/stresser/stresser.go
+++ src/tenta-dns/stresser/stresser.go
@@ -159,10 +159,9 @@ func (w *worker) doWork(id uint) {
 				c.Net = "tcp-tls"
 			}
 			c.SingleInflight = true
-			// c.DialTimeout = time.Second * 10
-			// c.WriteTimeout = time.Second * 10
-			// c.ReadTimeout = time.Second * 30
-			c.Timeout = time.Second * 300
+			c.DialTimeout = time.Second
+			c.WriteTimeout = time.Second * 3
+			c.ReadTimeout = time.Second * 30
 			in, _, err := c.Exchange(m, net.JoinHostPort(*ip, strconv.Itoa(int(*port))))
 			s := false
 			msg := ""
