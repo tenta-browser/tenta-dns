166989a4946a3fd443cc721ae1ba18b1ecbfbe64
shadrick <barna@mikandi.com>
2017-11-29 18:35:04 +0200

    Adding stack trace to panic feedback

--- src/tenta-dns/director/director.go
+++ src/tenta-dns/director/director.go
@@ -24,12 +24,10 @@ package director
 
 import (
 	"fmt"
-	"github.com/coreos/go-systemd/daemon"
-	"github.com/sirupsen/logrus"
-	"github.com/tevino/abool"
 	"net"
 	"os"
 	"path/filepath"
+	"runtime/debug"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -40,6 +38,10 @@ import (
 	"tenta-dns/responder"
 	"tenta-dns/runtime"
 	"time"
+
+	"github.com/coreos/go-systemd/daemon"
+	"github.com/sirupsen/logrus"
+	"github.com/tevino/abool"
 )
 
 const RECENT_FAILURE_LIMIT = 3
@@ -260,7 +262,7 @@ func (dir *Director) doOrchestrate(systemd bool) {
 			break
 		case f := <-failures:
 			dir.lg.Warnf("Got a failure in %s: %s", f.p.id, f.r)
-			dir.r.SlackWH.SendMessage(fmt.Sprintf("panic caught -- `[%s]`", f.r), f.p.id)
+			dir.r.SlackWH.SendMessage(fmt.Sprintf("panic caught -- ```[%s]\n---\n%s```", f.r, debug.Stack()), f.p.id)
 			fails := atomic.LoadUint32(&f.p.fails)
 			if fails >= RECENT_FAILURE_LIMIT {
 				dir.lg.Errorf("Perma killing %s", f.p.id)
