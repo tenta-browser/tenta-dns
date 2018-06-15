2162e7a220c8e4e1a84487e591542915f1c4a8de
shadrick <barna@mikandi.com>
2018-06-16 00:31:55 +0300

    Fixed formatting inconsistencies

--- responder/recursive_dns.go
+++ responder/recursive_dns.go
@@ -1867,7 +1867,7 @@ func handleDNSMessage(loggy *logrus.Entry, provider, network string, rt *runtime
 				prefix := fmt.Sprintf("[%s%s/%s]", prePrefix, rrt.domain, dns.TypeToString[rrt.record])
 				rrt.eventualLogger.FlushExt(rrt.l, prefix)
 			}
-			rrt.f.SendMessage(fmt.Sprintf("Cannot resolve [%s/%s] on [%s]", rrt.domain, dns.TypeToString[rrt.record]), operatorID)
+			rrt.f.SendMessage(fmt.Sprintf("Cannot resolve [%s/%s]", rrt.domain, dns.TypeToString[rrt.record]), operatorID)
 			result = setupResult(rrt, dns.RcodeServerFailure, nil)
 		}
 
@@ -1882,7 +1882,7 @@ func handleDNSMessage(loggy *logrus.Entry, provider, network string, rt *runtime
 				prefix := fmt.Sprintf("[%s%s/%s]", prePrefix, rrt.domain, dns.TypeToString[rrt.record])
 				rrt.eventualLogger.FlushExt(rrt.l, prefix)
 			}
-			rrt.f.SendMessage(fmt.Sprintf("Slow query for [%s/%s] on [%s]", rrt.domain, dns.TypeToString[rrt.record]), operatorID)
+			rrt.f.SendMessage(fmt.Sprintf("Slow query (%v) for [%s/%s] ", time.Now().Sub(startTime), rrt.domain, dns.TypeToString[rrt.record]), operatorID)
 		}
 
 		if !doWeReturnDNSSEC(rrt) && isDNSSECResponse(result) {
